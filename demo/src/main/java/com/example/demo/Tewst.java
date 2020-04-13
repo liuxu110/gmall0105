c3417968a75031b467be7238d4e9198d054fa80b
lsd110 <15658195209@163.com>
2020-04-13 12:18:42 +0800

    测试新增

---
+++ demo/src/main/java/com/example/demo/Tewst.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * @author lsd
+ * @create 2020-04-13 12:18
+ */
+public class Tewst {
+    public static void main(String[] args) {
+        System.out.println("测试删除");
+    }
+}
